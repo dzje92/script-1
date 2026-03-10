40571335175b865872859f075834f474855376e5
dzje92 <duongvanhongson710209@gmail.com>
2026-03-10 12:54:06 +0700

    Add files via upload
    Signed-off-by: dzje92 <duongvanhongson710209@gmail.com>

---
+++ memu.lua
@@ -0,0 +1,11 @@
+if getgenv().Menu then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/dzje92/script-1/refs/heads/main/Menu.VN.lua")()
+end
+
+if getgenv().Status then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/dzje92/status/refs/heads/main/status.lua")()
+end
+
+if getgenv().LeaveIfAdminBloxFruitJoin then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/710200999/FreeSeverVip.All/refs/heads/main/HopAdmin.BLF.V1.lua")()
+end
